42eecb33a16991ba6b059f136dd1d7c59217c8dd
km0313 <qutxzzz297@naver.com>
2023-03-16 11:04:39 +0900

    16194 solved

--- 16194.py
+++ 16194.py
@@ -4,9 +4,9 @@ import math
 read=sys.stdin.readline
 n=int(read())
 lis=list(map(int,read().split()))
-print(n,lis)
+
 dp=[math.inf]+lis
 for i in range(2,n+1):
     for j in range(1,i//2+1):
         dp[i]=min(dp[i],dp[j]+dp[i-j])
-print(dp)
+print(dp[-1])
